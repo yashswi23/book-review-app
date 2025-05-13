e1eac6dbab769148bf76dac91df7af4343baa59c
yashswi23 <yashswi2314@gmail.com>
2025-05-13 18:14:58 +0530

    Mongodb Connection Updation

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const mongoDB = mongoose.connect("mongodb://127.0.0.1:27017/serverside", {
+const mongoDB =mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+  })
 .then(() => console.log("MongoDB connected!"))
 .catch(err => console.log(err));
 
